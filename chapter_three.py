a732497650a11f567f65d8c917f3bc5d9b21938e
Nigel <nigel.vigan@gmail.com>
2020-10-07 19:45:42 -0700

    first commit

---
+++ chapter_three.py
@@ -0,0 +1,28 @@
+# odr, chr 
+
+
+
+
+print(chr(169))
+print(ord('©'))
+
+# print("car"<"cat")
+
+# a = 10
+# b = 5
+
+# c = (a + b) < (2 * a) # True
+
+# d = 1 > 5 # False
+# print("c", c)
+# print("d", d)
+
+# print(c and d) # if c is true and d is true, the entire expression will evaluate to true, else it will evaluate to false
+# print(c or d)
+# print(c is not d)
+
+# state = "MD"
+
+# result = state in ["MD", "CA", "TX", "NY"]
+
+# print("result", result)
